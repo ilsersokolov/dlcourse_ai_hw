ecfc15c3f0ac9ed762fa04e61425b7f3bf181fa3
Sokolov I.S <ilsersokolov@gmail.com>
2019-04-30 20:06:27 +0300

    some in asg2

--- assignment2/gradient_check.py
+++ assignment2/gradient_check.py
@@ -29,8 +29,11 @@ def check_gradient(f, x, delta=1e-5, tol=1e-4):
         analytic_grad_at_ix = analytic_grad[ix]
         numeric_grad_at_ix = 0
 
-        # TODO Copy from previous assignment
-        raise Exception("Not implemented!")
+        x1 = x.copy()
+        x1[ix] += delta
+        x2 = x.copy()
+        x2[ix] -= delta
+        numeric_grad_at_ix = ((f(x1)[0]-f(x2)[0])/(2*delta))
 
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (
--- assignment2/layers.py
+++ assignment2/layers.py
@@ -52,13 +52,18 @@ class Param:
 
 class ReLULayer:
     def __init__(self):
-        pass
+        self.X = None
 
     def forward(self, X):
         # TODO: Implement forward pass
         # Hint: you'll need to save some information about X
         # to use it later in the backward pass
-        raise Exception("Not implemented!")
+        # raise Exception("Not implemented!")
+        result = X.copy()
+        result[X < 0] = 0
+        self.X = result.copy()
+        self.X[result > 0] = 1
+        return result
 
     def backward(self, d_out):
         """
@@ -74,7 +79,8 @@ class ReLULayer:
         """
         # TODO: Implement backward pass
         # Your final implementation shouldn't have any loops
-        raise Exception("Not implemented!")
+        # raise Exception("Not implemented!")
+        d_result = self.X*d_out
         return d_result
 
     def params(self):
@@ -91,7 +97,10 @@ class FullyConnectedLayer:
     def forward(self, X):
         # TODO: Implement forward pass
         # Your final implementation shouldn't have any loops
-        raise Exception("Not implemented!")
+        # raise Exception("Not implemented!")
+        self.X = X
+        result = np.dot(X,self.W.value) + self.B.value
+        return result
 
     def backward(self, d_out):
         """
@@ -115,7 +124,10 @@ class FullyConnectedLayer:
         # It should be pretty similar to linear classifier from
         # the previous assignment
 
-        raise Exception("Not implemented!")
+        # raise Exception("Not implemented!")
+        d_input = np.dot(d_out,self.W.value.T)
+        self.W.grad += np.dot(self.X.T, d_out)
+        self.B.grad += np.sum(d_out, axis=0)
 
         return d_input
 
